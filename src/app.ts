9bc0a6dc70294528d7450451b6391d4c6cd20f11
Diego Chavez <ingdiegochavez18@gmail.com>
2023-03-07 19:23:31 -0500

    feat: :sparkles: dinamics-routes-implementation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { router } from "./routes";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Ready for the port: ${PORT}`));
