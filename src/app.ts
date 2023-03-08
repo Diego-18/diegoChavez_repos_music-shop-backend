fb8d1332b2ad8cc88ee53aa641edee1905de16f7
Diego Chavez <ingdiegochavez18@gmail.com>
2023-03-07 19:46:00 -0500

    feat: :sparkles: data-base-connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import db from "./config/database";
 import { router } from "./routes";
 
 const PORT = process.env.PORT || 4000;
@@ -8,4 +9,10 @@ const app = express();
 app.use(cors());
 app.use(router);
 
-app.listen(PORT, () => console.log(`Ready for the port: ${PORT}`));
+db()
+	.then(() =>
+		app.listen(PORT, () => console.log(`Ready for the port: ${PORT}`))
+	)
+	.catch((error) => {
+		console.error(error);
+	});
